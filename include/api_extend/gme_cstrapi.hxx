44c21070b5fa209efa59639aa89b876b037b0e2c
CastaneaG <18722189199@163.com>
2024-05-23 19:29:00 +0800

    feat: 新增 api extend 与 runtime virtual method 例程

---
+++ include/api_extend/gme_cstrapi.hxx
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <acis/dcl_cstr.h>
+
+#include <acis/acis.hxx>
+#include <acis/api.hxx>
+
+DECL_CSTR outcome api_gme_sweep_make_cuboid(double,  // x 方向的尺寸
+                                            double,  // y 方向的尺寸
+                                            double,  // z 方向的尺寸
+                                            BODY*&   // 构造的 Body
+);
---
+++ tests/runtime_virtual_method_test.cpp
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * @file    runtime_virtual_method_test.cpp
+ * @brief
+ * @details 属于 runtime virtual method 示例
+ * @author  CastaneaG
+ * @date    2024.5.21
+ *********************************************************************/
+
+// 测试用头文件
+#include <gtest/gtest.h>
+
+// ACIS
+#include <acis/cstrapi.hxx>
+#include <acis/get_top.hxx>
+#include <acis_utils/acis_utils.hpp>
+
+// GME
+#include "runtime_method/edge_count_args.hxx"
+
+// ====================================================================
+
+class runtime_virtual_method_test : public ::testing::Test {
+    int level = 0;
+
+  protected:
+    void SetUp() override { level = initialize_acis(); }
+
+    void TearDown() override { terminate_acis(level); }
+};
+
+TEST_F(runtime_virtual_method_test, EDGE_COUNT) {
+    BODY* body = nullptr;
+    int gme_edge_num = 0;
+    int acis_edge_num = 0;
+
+    API_BEGIN
+    api_make_cuboid(10.0, 10.0, 10.0, body);
+    // 调用count方法
+    logical ok = body->call_method(METHOD_ID("count", "edge_count_args"), EDGE_COUNT_ARGS(gme_edge_num));
+
+    ENTITY_LIST edge_list;
+    get_edges(body, edge_list);
+    acis_edge_num = edge_list.count();
+
+    API_END
+
+    EXPECT_TRUE(gme_edge_num == acis_edge_num);
+}
